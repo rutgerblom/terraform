05f4ef6752a567041eaa3cb0f7bbc0ce8379e9bc
Rutger Blom <serbl@SE-LND-SERBL.proact.local>
2020-02-08 19:50:44 +0100

    up

--- main.tf
+++ main.tf
@@ -33,59 +33,12 @@ data "vsphere_virtual_machine" "source_template" {
   datacenter_id = data.vsphere_datacenter.target_dc.id
 }
 
-##### Resources
-# Clones a single Linux VM from a template
-
-resource "vsphere_virtual_machine" "kubernetes_master" {
-  name             = "prod-k8s-master01"
-  resource_pool_id = data.vsphere_compute_cluster.target_cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.target_datastore.id
-  folder           = var.deploy_vsphere_folder
-
-  num_cpus = var.guest_vcpu
-  memory   = var.guest_memory
-  guest_id = data.vsphere_virtual_machine.source_template.guest_id
-
-  scsi_type = data.vsphere_virtual_machine.source_template.scsi_type
-
-  network_interface {
-    network_id   = data.vsphere_network.target_network.id
-    adapter_type = data.vsphere_virtual_machine.source_template.network_interface_types[0]
-  }
-
-  disk {
-    label            = "disk0"
-    size             = data.vsphere_virtual_machine.source_template.disks[0].size
-    eagerly_scrub    = data.vsphere_virtual_machine.source_template.disks[0].eagerly_scrub
-    thin_provisioned = data.vsphere_virtual_machine.source_template.disks[0].thin_provisioned
-  }
-
-  clone {
-    template_uuid = data.vsphere_virtual_machine.source_template.id
-
-    customize {
-      linux_options {
-        host_name = "prod-k8s-master01"
-        domain    = var.guest_domain
-      }
-
-      network_interface {
-        ipv4_address = "10.67.31.11"
-        ipv4_netmask = var.guest_ipv4_netmask
-      }
-
-      ipv4_gateway    = var.guest_ipv4_gateway
-      dns_server_list = [var.guest_dns_servers]
-      dns_suffix_list = [var.guest_dns_suffix]
-    }
-  }
-}
-
+##### Resourcesxs
 # Clones multiple Linux VMs from a template
 
-resource "vsphere_virtual_machine" "kubernetes_workers" {
+resource "vsphere_virtual_machine" "vsphere_vms" {
   count            = 3
-  name             = "prod-k8s-worker0${count.index + 1}"
+  name             = "vm${count.index + 1}"
   resource_pool_id = data.vsphere_compute_cluster.target_cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.target_datastore.id
   folder           = var.deploy_vsphere_folder
@@ -113,12 +66,12 @@ resource "vsphere_virtual_machine" "kubernetes_workers" {
 
     customize {
       linux_options {
-        host_name = "prod-k8s-worker0${count.index + 1}"
+        host_name = "vm${count.index + 1}"
         domain    = var.guest_domain
       }
 
       network_interface {
-        ipv4_address = "10.67.31.${21 + count.index}"
+        ipv4_address = "10.2.129.${210 + count.index}"
         ipv4_netmask = var.guest_ipv4_netmask
       }
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,21 +1,21 @@
 # Provider
-provider_vsphere_host = "vcsa01-z67.sddc.lab"
+provider_vsphere_host = "vcenter.demo.local"
 provider_vsphere_user = "administrator@vsphere.local"
 provider_vsphere_password = "VMware1!"
 
 # Infrastructure
-deploy_vsphere_datacenter = "MGMT-Z67"
-deploy_vsphere_cluster = "CL01-Z67"
-deploy_vsphere_datastore = "vsanDatastore"
-deploy_vsphere_folder = "/kubernetes"
-deploy_vsphere_network = "PG-10.67.31.0"
+deploy_vsphere_datacenter = "Datacenter"
+deploy_vsphere_cluster = "Cluster"
+deploy_vsphere_datastore = "demo_nfs01"
+deploy_vsphere_folder = "/Dummies"
+deploy_vsphere_network = "vlan-301"
 
 # Guest
-guest_template = "packer-ubuntu-18.04"
+guest_template = "ubuntu_18.04"
 guest_vcpu = "2"
 guest_memory = "2048"
 guest_ipv4_netmask = "24"
-guest_ipv4_gateway = "10.67.31.254"
-guest_dns_servers = "10.67.10.5"
-guest_dns_suffix = "pod03.lab.local"
-guest_domain = "sddc.lab"
+guest_ipv4_gateway = "10.2.129.1"
+guest_dns_servers = "10.2.129.10"
+guest_dns_suffix = "demo.local"
+guest_domain = "demo.local"
