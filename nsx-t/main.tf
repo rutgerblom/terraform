4860a3bae62335c8dee020ead95360d9e2155c22
Rutger Blom <serbl@SE-LND-SERBL.proact.local>
2020-02-09 16:02:25 +0100

    up

--- nsx-t/main.tf
+++ nsx-t/main.tf
@@ -3,9 +3,9 @@
 # REST API running on the NSX manager.
 #
 provider "nsxt" {
-  host                  = "${var.nsx_manager}"
-  username              = "${var.nsx_username}"
-  password              = "${var.nsx_password}"
+  host                  = var.nsx_manager
+  username              = var.nsx_username
+  password              = var.nsx_password
   allow_unverified_ssl  = true
   max_retries           = 10
   retry_min_delay       = 500
@@ -46,8 +46,8 @@ resource "nsxt_policy_segment" "segment1" {
   transport_zone_path = "/infra/sites/default/enforcement-points/default/transport-zones/12d25875-7960-4ceb-91f1-f39a24a57014"
 
   tag {
-    scope = "${var.nsx_tag_scope}"
-    tag   = "${var.nsx_tag}"
+    scope = var.nsx_tag_scope
+    tag   = var.nsx_tag
   }
 
   tag {
