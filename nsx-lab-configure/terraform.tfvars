e40b0b2d947642adf48f81d309a2d6a074a420bb
Rutger Blom <serbl@SE-LND-SERBL.proact.local>
2020-04-06 23:24:33 +0200

    up

---
+++ nsx-lab-configure/terraform.tfvars
@@ -0,0 +1,5 @@
+nsx_manager = "nsxmanager01.lab.local"
+nsx_username = "admin"
+nsx_password = "VMware1!VMware1!"
+nsx_tag_scope = "manager"
+nsx_tag = "terraform"
---
+++ nsx-lab-configure/variables.tf
@@ -0,0 +1,15 @@
+variable "nsx_manager" {
+  description = "NSX Manager FQDN or IP"
+}
+variable "nsx_username" {
+  description = "NSX Manager user"
+}
+variable "nsx_password" {
+  description = "NSX Manager password"
+}
+variable "nsx_tag_scope" {
+  description = "default scope"
+}
+variable "nsx_tag" {
+  description = "default tag"
+}
